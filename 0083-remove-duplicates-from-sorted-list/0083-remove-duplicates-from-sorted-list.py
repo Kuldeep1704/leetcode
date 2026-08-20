ed6dcaf190e15a09f42d02e1bf320af7da97a50d
Kuldeep1704 <kuldeepkkr2025@gmail.com>
2026-08-20 23:58:27 +0530

    Time: 3 ms (22.96%), Space: 19.1 MB (93.53%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -0,0 +1,13 @@
+class Solution:
+    def deleteDuplicates(self, head):
+
+        temp = head
+
+        while temp and temp.next:
+
+            if temp.val == temp.next.val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+
+        return head
